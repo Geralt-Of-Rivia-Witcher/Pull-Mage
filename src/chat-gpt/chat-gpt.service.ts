b8cd4963669bb20f2606b02a0932b0311bb0d4bb
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2024-03-17 00:15:12 +0530

    refactor: :children_crossing: improved GPT prompt for better response

--- src/chat-gpt/chat-gpt.service.ts
+++ src/chat-gpt/chat-gpt.service.ts
@@ -34,38 +34,38 @@ export class ChatGptService {
   }
 
   private async getPrReview(fileChanges: string): Promise<string> {
-    // const completion = await this.openai.chat.completions.create({
-    //   messages: [
-    //     {
-    //       role: 'system',
-    //       content:
-    //         'You are reviewing a pull request and suggesting changes. Your role is to ensure code quality, correctness, and alignment with project standards.',
-    //     },
-    //     {
-    //       role: 'system',
-    //       content:
-    //         'Consider everything from variable name changes to logic improvement, code quality, design patterns, performance, security, dependencies, and more.',
-    //     },
-    //     {
-    //       role: 'system',
-    //       content:
-    //         'Please provide detailed suggestions for improvements, covering all aspects of the code changes. Each change includes information such as filename, status (modified/added/deleted), additions, deletions, optional previous_filename and the patches containing the actual code changes.',
-    //     },
-    //     {
-    //       role: 'system',
-    //       content:
-    //         'Avoid generating new code and instead provide clear and detailed recommendations for the author to implement.',
-    //     },
-    //     {
-    //       role: 'user',
-    //       content: fileChanges,
-    //     },
-    //   ],
-    //   model: 'gpt-3.5-turbo',
-    // });
+    const completion = await this.openai.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are reviewing a pull request and suggesting changes. Your role is to ensure code quality and correctness, and alignment with coding standards.',
+        },
+        {
+          role: 'system',
+          content:
+            'Please provide detailed suggestions for improvements directly related to the changes made in the pull request. Focus on ensuring that the modifications align with project standards and maintain code quality.',
+        },
+        {
+          role: 'system',
+          content:
+            'Consider the specific changes made in the pull request, including variable name changes, logic improvements, code quality enhancements, and adherence to design patterns.',
+        },
+        {
+          role: 'system',
+          content:
+            'Avoid generating new code and instead provide clear and detailed recommendations for the author to implement within the context of the existing changes.',
+        },
+        {
+          role: 'user',
+          content: fileChanges,
+        },
+      ],
+      model: 'gpt-3.5-turbo',
+    });
 
-    // return completion.choices[0].message.content;
-    return 'This is a placeholder for the GPT response to save credits. #MaiGareebHoon';
+    return completion.choices[0].message.content;
+    // return 'This is a placeholder for the GPT response to save credits. #MaiGareebHoon';
   }
 
   private async getCodeExplanation(fileChanges: string): Promise<string> {
@@ -74,17 +74,17 @@ export class ChatGptService {
         {
           role: 'system',
           content:
-            'Please provide a detailed explanation of the code changes included in the current pull request. Each change includes information such as filename, status (modified/added/deleted), additions, deletions, optional previous_filename and the patches containing the actual code changes.',
+            "Please provide an overview of the code changes included in the current pull request. Summarize the purpose and significance of these changes, including how they contribute to the project's functionality, correctness, and quality.",
         },
         {
           role: 'system',
           content:
-            'Describe the purpose of the changes, the problem they aim to solve, and any relevant context or background information. Explain how each change contributes to the overall functionality, correctness, and quality of the codebase.',
+            'Highlight the main objectives or goals achieved by these changes, the problems they solve, and any notable improvements or optimizations made to the codebase.',
         },
         {
           role: 'system',
           content:
-            'Break down the changes into manageable chunks and provide clear explanations for each modification.',
+            'Offer a high-level breakdown of the modifications, grouping related changes together and explaining their collective impact on the project.',
         },
         {
           role: 'user',
@@ -106,17 +106,12 @@ export class ChatGptService {
         {
           role: 'system',
           content:
-            'You are provided with a GitHub pull request file changes. Each change includes information such as filename, status (modified/added/deleted), additions, deletions, optional previous_filename and the patches containing the actual code changes.',
+            'You are provided with a GitHub pull request file changes. Each change includes information such as filename, status (modified/added/deleted), additions, deletions, optional previous_filename, and the patches containing the actual code changes.',
         },
         {
           role: 'system',
           content:
-            'Now, you need to answer a question related to the code changes. I will provide you with the question, and you need to answer it based on the provided file changes.',
-        },
-        {
-          role: 'system',
-          content:
-            'Break down the answer into manageable chunks and provide clear explanations.',
+            'Now, you need to answer a question related to the code changes. I will provide you with the question, and you need to answer it based on the provided file changes. Your response should focus on directly addressing the question with concise information extracted from the file changes.',
         },
         {
           role: 'user',
--- src/pull-request-event/pull-request-event.service.ts
+++ src/pull-request-event/pull-request-event.service.ts
@@ -25,8 +25,15 @@ export class PullRequestEventService {
   }
 
   async sendIntroductionMessage(ctx: IPullRequestEvent) {
-    const message = `🤖 Hello! I'm a bot designed to assist with pull request reviews.
-    To trigger my review, please add a comment containing the word "/review".`;
+    const message =
+      '\n🤖 **Greetings, coder! I am Pull Mage, your mighty Pull Request Assistant!** 🚀\n\n' +
+      "I'm here to help you with your PRs by providing reviews, explanations, and answering your questions.\n\n" +
+      "Here's what I can do:\n\n" +
+      '1. **Review your PR**: Type `/review` to request a review of your pull request.\n\n' +
+      '2. **Explain your PR**: Type `/explain` to get a detailed explanation of the changes included in your pull request.\n\n' +
+      "3. **Ask a question**: Type `/ask = ` followed by your question to get answers related to your pull request. Provide the question after the '=' sign, and Pull Mage will do its best to answer within the context of your PR.\n\n" +
+      "Feel free to interact with me whenever you need assistance with your PRs! Simply use one of the commands above, and I'll be at your service.\n\n" +
+      'Happy coding! 🚀';
     this.gitHubService.postCommentOnPullRequest({
       ...ctx,
       message,
