47bffd805da0b23cc11eb2c431b77d68c2ed5d6d
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2024-03-17 19:39:39 +0530

    refactor: :truck: removed unnecessary POST route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
+  @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.healthCheck();
   }
 
   @Get('/health-check')
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-
   healthCheck(): string {
     return 'Pew Pew Pew 🔫🔫🔫';
   }
